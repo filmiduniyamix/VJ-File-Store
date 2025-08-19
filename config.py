89d5d3abd958fd6edc9ad2fa5def42ecfefbb1f0
filmiduniyamix <filmiduniyamix@gmail.com>
2025-08-19 12:37:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -58,12 +58,12 @@ BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 
 # Verify Info :-
-VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
+VERIFY_MODE = bool(environ.get('VERIFY_MODE', True)) # Set True or False
 
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "publicearn.com") # shortlink domain without https://
-SHORTLINK_API = environ.get("SHORTLINK_API", "de08290b12d9e34191f3a057070a50a18187fff3") # shortlink api
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/DowPCMOffcL/13") # how to open link 
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "gplinks.co") # shortlink domain without https://
+SHORTLINK_API = environ.get("SHORTLINK_API", "f4b378dd4a30666fd69df89709c55d4f694b5826") # shortlink api
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/bots_tutorial_all") # how to open link 
 
 # Website Info:
 WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', True)) # Set True or False
